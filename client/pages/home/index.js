061b2d0c2a40ae1c0a66af4fa466334de4606809
liuziyang <liuziyang@youzan.com>
2019-10-15 14:13:11 +0800

    更新

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
