a21d78e5f9510cf3193445fe996e7834dec36cf6
liuziyang <liuziyang@youzan.com>
2019-10-18 14:47:51 +0800

    fix: 更新目录配置

--- client/webpack/config/dev.config.js
+++ client/webpack/config/dev.config.js
@@ -1,9 +1,11 @@
 const merge = require('webpack-merge');
 const FriendlyErrorsWebpackPlugins = require('friendly-errors-webpack-plugin');
-const baseDevConfig = require('./base.dev.config');
 const scanEntries = require('../utils/scan-entries');
 const helpers = require('../utils/helper');
+const baseConfig = require('./base.config');
 const config = {
+  mode: "development",
+  devtool: 'cheap-module-source-map',
   devServer: {
     host: '127.0.0.1',
     compress: true,
@@ -25,4 +27,4 @@ const config = {
 
 config.entry = scanEntries(helpers.getAppRoot());
 
-module.exports = merge.smart(baseDevConfig, config);
+module.exports = merge.smart(baseConfig, config);
--- client/webpack/config/prod.config.js
+++ client/webpack/config/prod.config.js
@@ -1,17 +1,79 @@
 const merge = require('webpack-merge');
-const path = require('path');
-const baseConfig = require('./base.config');
-const baseProdConfig = require('./base.prod.config');
-const scanEntries = require('../utils/scan-entries');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const defaultPreset = require('cssnano-preset-default');
+const autoprefixer = require('autoprefixer');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const baseConfig = require('./base.prod.config');
 const helpers = require('../utils/helper');
 const ResourceConfigGeneratePlugin = require('../plugins/webpack-resource-config-generate-plugin');
 
-const config = merge.smart(
-  baseConfig,
-  {
-    entry: scanEntries(helpers.getAppRoot())
-  },
-  baseProdConfig,
-);
+const isAnalyze = process.argv.indexOf('analyze') > -1;
 
-module.exports = config;
+const plugins = [new ResourceConfigGeneratePlugin()];
+
+if (isAnalyze) {
+  plugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = merge.smart(baseConfig, {
+  mode: 'production',
+  entry: scanEntries(helpers.getAppRoot()),
+  devtool: 'none',
+  optimization: {
+    namedModules: true,
+    moduleIds: 'hashed',
+
+    namedChunks: true,
+    chunkIds: 'named',
+    minimizer: [
+      new CleanWebpackPlugin(),
+      new TerserPlugin({
+        parallel: true,
+        sourceMap: false,
+      }),
+  
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorPluginOptions: {
+          preset: cssnanoProdPreset,
+        }
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        commons: {
+          chunks: 'all',
+          name: "commons",
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+        vendors: {
+          chunks: 'initial',
+          name: 'vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        }
+      }
+    }
+  },
+  plugins,
+});
+
+function cssnanoProdPreset(opt = {}) {
+  const options = Object.assign({
+      autoprefixer: {
+        cascade: false,
+      }
+    },
+    opt
+  );
+
+  const plugins = [...defaultPreset(options).plugins, [autoprefixer, options.autoprefixer]]
+
+  return {
+    plugins
+  }
+}
